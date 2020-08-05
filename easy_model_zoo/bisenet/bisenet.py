499a32d776f005ab055af6a2f824f7cbd6e499d0
SharifElfouly <selfouly@gmail.com>
2020-08-05 22:47:41 +0200

    cleaned up

--- easy_model_zoo/bisenet/bisenet.py
+++ easy_model_zoo/bisenet/bisenet.py
@@ -443,10 +443,3 @@ class BisenetModel(Model):
         mask.show()
         return mask
 
-def interval_mapping(image, from_min, from_max, to_min, to_max):
-    # map values from [from_min, from_max] to [to_min, to_max]
-    # image: input array
-    from_range = from_max - from_min
-    to_range = to_max - to_min
-    scaled = np.array((image - from_min) / float(from_range), dtype=float)
-    return to_min + (scaled * to_range)
